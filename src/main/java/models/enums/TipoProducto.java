4829306a0527c0b63b4362a2015bcb972212a7ce
Neifer <neiferflorian532@gmail.com>
2026-07-01 05:11:31 +0200

    Titulo a mostrar en el enum de tipo de producto.

--- src/main/java/models/enums/TipoProducto.java
+++ src/main/java/models/enums/TipoProducto.java
@@ -1,9 +1,18 @@
 package models.enums;
 
 public enum TipoProducto {
-    TECNOLOGIA,
-    HERRAMIENTA,
-    MUEBLE,
-    UTENSILIO,
-    PERSONAL
+    TECNOLOGIA("Tecnología"),
+    HERRAMIENTA("Herramientas"),
+    MUEBLE("Muebles"),
+    UTENSILIO("Utensilios"),
+    PERSONAL("Personales");
+
+    private final String tituloTipoProducto;
+
+    TipoProducto(String tituloTipoProducto) {
+        this.tituloTipoProducto = tituloTipoProducto;
+    }
+    public String getTituloTipoProducto() {
+        return tituloTipoProducto;
+    }
 }
